1b6af0d454a76c988c4a35e8cac4b75541d15868
Wataru MIYAGUNI <gonngo@gmail.com>
2014-05-15 22:30:01 +0900

    Add api test 'PUT /reports/:yyyymmdd'

---
+++ spec/api/nicocal/api_spec.rb
@@ -0,0 +1,45 @@
+require 'spec_helper'
+
+describe Nicocal::API do
+  describe "PUT /reports/:yyyymmdd" do
+    context 'when given date and mood_id' do
+      let(:mood) { Mood.create(name: 'happiness', score: 100) }
+
+      it 'save successfully report of day' do
+        expect {
+          put '/reports/19930224', { mood_id: mood.id }
+        }.to change(Report, :count).by(1)
+      end
+
+      context 'and mood is already registered on date' do
+        before do
+          Report.create(date: '1993-02-24', mood: mood)
+        end
+
+        it 'update successfully report of day' do
+          expect {
+            put '/reports/19930224', { mood_id: mood.id }
+          }.not_to change(Report, :count).by(1)
+        end
+      end
+    end
+
+    context 'when given invalid date' do
+      before { put '/reports/123456' }
+      it { expect(response).not_to be_success }
+    end
+
+    context 'when given invalid mood id' do
+      before { put '/reports/19930224', { mood_id: 'foo' } }
+      it { expect(response).not_to be_success }
+    end
+
+    context 'when given mood id does not exist' do
+      before { put '/reports/19930224', { mood_id: 1 } }
+      it 'returns code `400` and detail error message' do
+        expect(response).not_to be_success
+        expect(JSON.parse(response.body)).to have_key 'error'
+      end
+    end
+  end
+end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -8,4 +8,8 @@ ActiveRecord::Migration.maintain_test_schema!
 RSpec.configure do |config|
   config.use_transactional_fixtures = true
   config.order = 'random'
+
+  config.include RSpec::Rails::RequestExampleGroup, type: :request, example_group: {
+    file_path: /spec\/api/
+  }
 end
