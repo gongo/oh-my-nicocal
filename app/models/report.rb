6c35dc4ffaaffedc1b0b92b673b240d10cd84e3e
Wataru MIYAGUNI <gonngo@gmail.com>
2014-05-15 20:48:20 +0900

    Add Report.of test.

--- app/models/report.rb
+++ app/models/report.rb
@@ -1,3 +1,21 @@
 class Report < ActiveRecord::Base
   belongs_to :mood
+
+  default_scope { order(:date) }
+
+  class << self
+    #
+    # Returns reports of +year_and_month+
+    #
+    # @param  [String]  year_and_month  %Y/%m
+    # @return [ActiveRecord::Relation]
+    #
+    # @example
+    #   Report.of('2014/06') # => #<ActiveRecord::Relation [#<Report ..>]>
+    #
+    def of(year_and_month)
+      date = year_and_month.to_date
+      where(date: date.beginning_of_month..date.end_of_month)
+    end
+  end
 end
--- spec/models/report_spec.rb
+++ spec/models/report_spec.rb
@@ -8,4 +8,38 @@ describe Report do
   subject { @report }
 
   it { should respond_to(:mood) }
+
+  describe '.of' do
+    before do
+      today = Date.today
+      Report.create(date: today.prev_month.end_of_month)
+      Report.create(date: today.beginning_of_month)
+      Report.create(date: today.end_of_month)
+      Report.create(date: today.next_month.beginning_of_month)
+    end
+
+    context 'when given date which mood data is exists' do
+      subject { Report.of(Date.today.strftime('%Y/%m')) }
+
+      it 'return matches reports.' do
+        expect(subject.count).to eq 2
+      end
+    end
+
+    context 'when given date which mood data is not exists' do
+      subject { Report.of(Date.today.prev_year.strftime('%Y/%m')) }
+
+      it 'not return' do
+        expect(subject).to be_empty
+      end
+    end
+
+    context 'when given invalid date' do
+      subject { Report.of('foobar') }
+
+      it 'raise Argument Error' do
+        expect { subject }.to raise_error ArgumentError
+      end
+    end
+  end
 end
